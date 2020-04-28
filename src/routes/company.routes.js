506f65cc835c87a4fe74c8b691782d1449a4d0ab
Jairo Salazar <jairosalazar@MacBook-Pro.local>
2020-04-28 17:05:01 -0500

    funcion listar compañias

--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -1,4 +1,5 @@
 const Company = require('../controllers/company.controller');
 module.exports = (router) => {
 	router.post('/create-company', Company.createCompany);
+	router.post('/list-companies', Company.listCompanies);
 };
