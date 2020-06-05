09b66ae4a400e1caa40cbb7088ea6531426ccdcb
Alejandro Garcia <jhonalejandro00@gmail.com>
2020-06-05 17:58:25 -0500

    cambios garcia ultimo sprint

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,4 +1,5 @@
 const Order = require('../controllers/order.controller');
 module.exports = (router) => {
 	router.post('/create-order', Order.createOrder);
+	router.get('/orderbyclient/:name', Order.findOrderByClient);
 };
