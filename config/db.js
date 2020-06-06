67c4a6f93396b75831e4293b3efbe9d6e968fb38
stol07 <santiago.lmarin07@gmail.com>
2020-06-06 01:33:04 -0500

    modificar categoria producto

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dbURL = require('./properties').DB;
 
 module.exports = () => {
-	mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+	mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true })
 		.then(() => console.log(`Mongo connected on ${dbURL}`))
 		.catch(err => console.log(`Connection has error ${err}`))
 
--- src/routes/product_category.routes.js
+++ src/routes/product_category.routes.js
@@ -2,4 +2,5 @@ const ProductCategory = require('../controllers/product_category.controller');
 module.exports = (router) => {
 	router.post('/create-prod_cat', ProductCategory.create);
 	router.get('/prod-categories', ProductCategory.read);
+	router.put('/prod-categories/:id', ProductCategory.update);
 }
